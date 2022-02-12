2caafdc4c15518e32d9c6b1a8c3bd766f53170fa
Carlos Segura <lithos.contact@gmail.com>
2022-02-12 13:51:48 +0100

    Working on spreadsheet table

--- src/components/SpreadSheet.vue
+++ src/components/SpreadSheet.vue
@@ -1,15 +1,63 @@
 <template>
-<div>
-  <h1>SpreadSheet</h1>
-</div>
+  <div>
+    <table class="mx-auto">
+      <thead>
+        <th v-for="(i, index) in numHeaders" :key="'A' + index">COLUMN {{ index + 1 }}</th>
+      </thead>
+      <tbody>
+        <tr v-for="(i, index) in numRows" :key="'B' + index">
+          <td v-for="(i, index) in numCells" :key="'C' + index">
+            <input class="table__spreadsheet-input" />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-export default {
+import { ref } from 'vue';
 
+export default {
+  setup() {
+    const numHeaders = ref(5);
+    const numRows = ref(10);
+    const numCells = ref(5);
+
+    return {
+      numHeaders,
+      numRows,
+      numCells,
+    };
+  },
 };
 </script>
 
-<style>
+<style lang="scss">
+@import "@/scss/app.scss";
+@import "@/scss/variables.scss";
+table {
+  border: 1px solid white;
+  background: #50505050;
+}
 
+th {
+  border: 1px solid rgba(255, 174, 0, 0.438);
+  padding: 5px;
+}
+
+td {
+  border: 2px solid rgba(0, 255, 255, 0.438);
+  padding: 0;
+  margin: 0;
+}
+
+input:focus {
+  outline: none;
+}
+
+.table__spreadsheet-input {
+  padding: 0;
+  margin: 0;
+}
 </style>
